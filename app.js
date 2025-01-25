9ea3da12d126e9e0cc795697e6684c03eb01695c
owaiskhalsai13 <owaiskhalsai@gmail.com>
2025-01-25 15:11:01 +0500

    stop watch completed

--- app.js
+++ app.js
@@ -4,25 +4,48 @@ const hourcontainer = document.querySelector(".hour")
 let second = 0
 let minute = 0
 let hour = 0
-function start() {
+function startWatch()  {
     console.log("start watch");
-    setInterval(function()  {
-        if (second === 60){
+    stopwatch = setInterval(function() {
+        second +=1;
+        if(second<10){
+            secondcontainer.innerHTML="0" + second
+        }
+        else{
+            secondcontainer.innerHTML=second
+        }
+        if(second == 60){
             second = 0;
             minute += 1;
-            minutecontainer.innerHTML = minute;
-
-            return
+            
+            if(minute<10){
+                minutecontainer.innerHTML="0" + minute
+            }
+            else{
+                minutecontainer.innerHTML=minute
+            }
         }
-        second += 1;
-        if (second < 10){
-            secondcontainer.innerHTML = "0" + second
-            return
-        
+        if(minute == 60){
+            hour += 1;
+            minute = 0;
+            if(hour<10){
+                hourcontainer.innerHTML="0" + hour
+            }
+            else{
+                hourcontainer.innerHTML=hour
+            }
         }
-        secondcontainer.innerHTML = second;
-        
-    }, 100);
-    
-
+    },100)
+}
+function stop(){
+    clearInterval(stopwatch)
+}
+function refresh(){
+    clearInterval(stopwatch)
+    second=0;
+    minute=0;
+    hour=0;
+    secondcontainer.innerHTML= "00";
+    minutecontainer.innerHTML= "00";
+    hourcontainer.innerHTML= "00";
 }
